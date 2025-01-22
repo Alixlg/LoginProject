2be62b08fdcd5e0a107cb7bc4c557563666ecafc
Alireza <alirezatorkaman.kaman@gmail.com>
2025-01-22 11:30:31 +0330

    initial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './+components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'Login', component: LoginComponent, pathMatch: 'full' },
+  { path: '', redirectTo: '/Login', pathMatch: 'full' }
+];
